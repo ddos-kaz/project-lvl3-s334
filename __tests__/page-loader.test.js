a469a18d0ab2d08f7425cb37e2dc7ba4070af8bc
Damir <damir.doszhan@gmail.com>
2018-10-16 20:50:39 +0600

    Solving problems of travis

--- __tests__/page-loader.test.js
+++ __tests__/page-loader.test.js
@@ -6,22 +6,26 @@ import fs from 'mz/fs';
 import path from 'path';
 import loader from '../src';
 
+const host = 'http://localhost';
+const pathname = '/test';
+axios.defaults.host = host;
+axios.defaults.adapter = httpAdapter;
+
 describe('Page Loader', () => {
-  const host = 'http://localhost';
-  const pathname = '/test/second';
-  axios.defaults.host = host;
-  axios.defaults.adapter = httpAdapter;
   beforeAll(async () => {
     const testData = await fs.readFile(path.resolve(__dirname, '__fixtures__', 'simple_page.html'), 'utf8');
     nock(host).get(pathname).reply(200, testData);
   });
   it('Simple loading page content', async () => {
-    const fileName = 'localhost-test-second.html';
+    const fileName = 'localhost-test.html';
     const tempDir = await fs.mkdtemp(path.join(os.tmpdir(), 'foo-'));
     await loader(`${host}${pathname}`, tempDir);
     const responseData = await fs.readFile(path.resolve(tempDir, fileName), 'utf8');
-    axios.get(`${host}${pathname}`).then((res) => {
-      expect(responseData).toEqual(res.data);
-    }).catch(err => console.log(err));
+    const sourceData = await fs.readFile(path.resolve(__dirname, '__fixtures__', 'simple_page.html'), 'utf8');
+    expect(responseData).toBe(sourceData);
+  });
+
+  afterAll(() => {
+    nock.cleanAll();
   });
 });
