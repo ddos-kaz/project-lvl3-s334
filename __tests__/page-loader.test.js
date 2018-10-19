3b2b85c69080467d05f64ff533d5a747732ad7b3
Damir <damir.doszhan@gmail.com>
2018-10-20 00:32:37 +0600

    2nd step

--- __tests__/page-loader.test.js
+++ __tests__/page-loader.test.js
@@ -8,21 +8,40 @@ import loader from '../src';
 
 const host = 'http://localhost';
 const pathname = '/test';
-//  axios.defaults.host = host;
 axios.defaults.adapter = httpAdapter;
 
 describe('Page Loader', () => {
   beforeAll(async () => {
-    const testData = await fsPromises.readFile(path.resolve(__dirname, '__fixtures__', 'simple_page.html'), 'utf8');
-    nock(host).get(pathname).reply(200, testData);
+    const sourceHTML = await fsPromises.readFile(path.resolve(__dirname, '__fixtures__', 'source.html'), 'utf8');
+    const sourceImg = await fsPromises.readFile(path.resolve(__dirname, '__fixtures__/src', 'hexlet_logo.png'));
+    const sourceScript = await fsPromises.readFile(path.resolve(__dirname, '__fixtures__/src', 'script.js'), 'utf8');
+    const sourceCSS = await fsPromises.readFile(path.resolve(__dirname, '__fixtures__/src', 'style.css'), 'utf8');
+    nock(host)
+      .get(pathname)
+      .reply(200, sourceHTML)
+      .get(`${pathname}/src/hexlet_logo.png`)
+      .reply(200, sourceImg)
+      .get(`${pathname}/src/script.js`)
+      .reply(200, sourceScript)
+      .get(`${pathname}/src/style.css`)
+      .reply(200, sourceCSS);
   });
-  it('Simple loading page content', async () => {
+  it('Transforming html file and downloading resources', async () => {
     const fileName = 'localhost-test.html';
+    const dirName = 'localhost-test_files';
     const tempDir = await fsPromises.mkdtemp(path.join(os.tmpdir(), 'foo-'));
     await loader(`${host}${pathname}`, tempDir);
+    const sourceImg = await fsPromises.readFile(path.resolve(__dirname, '__fixtures__/src', 'hexlet_logo.png'));
+    const responseImg = await fsPromises.readFile(`${tempDir}/${dirName}/src-hexlet_logo.png`, 'utf8');
     const responseData = await fsPromises.readFile(path.resolve(tempDir, fileName), 'utf8');
-    const sourceData = await fsPromises.readFile(path.resolve(__dirname, '__fixtures__', 'simple_page.html'), 'utf8');
-    expect(responseData).toBe(sourceData);
+    const responseScript = await fsPromises.readFile(`${tempDir}/${dirName}/src-script.js`, 'utf8');
+    const responseCSS = await fsPromises.readFile(`${tempDir}/${dirName}/src-style.css`, 'utf8');
+    const sourceScript = await fsPromises.readFile(path.resolve(__dirname, '__fixtures__/src', 'script.js'), 'utf8');
+    const sourceCSS = await fsPromises.readFile(path.resolve(__dirname, '__fixtures__/src', 'style.css'), 'utf8');
+    expect(responseData).toMatchSnapshot();
+    expect(responseCSS).toBe(sourceCSS);
+    expect(responseImg.toString()).toBe(sourceImg.toString());
+    expect(responseScript).toBe(sourceScript);
   });
 
   afterAll(() => {
--- src/index.js
+++ src/index.js
@@ -3,11 +3,60 @@ import fs from 'mz/fs';
 import url from 'url';
 import _ from 'lodash';
 import path from 'path';
+import cheerio from 'cheerio';
+
+const generateName = nameList => _.join(_.compact(nameList), '-');
+
+const loadData = address => axios.get(address)
+  .then(response => response.data)
+  .catch(err => Promise.reject(new Error(`Error in laoding html : ${err}, from url: ${address}`)));
+
+const loadResources = (address, dirName) => axios.get(address, { responseType: 'arraybuffer' })
+  .then(response => fs.writeFile(dirName, response.data))
+  .catch(err => Promise.reject(new Error(`Error in loading resources: ${err}, from url: ${address}`)));
+
+const generateResourceName = (resourcePath) => {
+  const { dir, name, ext } = path.parse(resourcePath);
+  return `${generateName(_.split(path.resolve(dir, name), '/'))}${ext}`;
+};
+
+const transformData = (data, dirName) => {
+  const $ = cheerio.load(data);
+  const tags = [
+    { tagName: 'script', tagSrc: 'src' },
+    { tagName: 'img', tagSrc: 'src' },
+    { tagName: 'link', tagSrc: 'href' },
+  ];
+  const resourceLinks = tags.reduce((acc, tag) => {
+    const links = $('html').find(tag.tagName);
+    const mappedLinks = links.map((i) => {
+      if (!$(links[i]).attr(tag.tagSrc)) {
+        return '';
+      }
+      const resourcePath = $(links[i]).attr(tag.tagSrc);
+      $(links[i]).attr(tag.tagSrc, `${dirName}/${generateResourceName(resourcePath)}`);
+      return resourcePath;
+    });
+    return [...acc, ...mappedLinks];
+  }, []);
+  return Promise.resolve({ formattedData: $.html(), resourceLinks: _.compact(resourceLinks) });
+};
+
+const generateResourceDirName = (dir, base) => path.normalize(path.format({ dir, base }));
 
 export default (address, dir) => {
   const { hostname, pathname } = url.parse(address);
-  const fileName = `${_.join(_.compact(_.concat(_.split(hostname, '.'), _.split(pathname, '/'))), '-')}.html`;
-  return axios.get(address)
-    .then(response => fs.writeFile(path.resolve(dir, fileName), response.data))
-    .catch(err => err);
+  const name = generateName([..._.split(hostname, '.'), ..._.split(pathname, '/')]);
+  const fileName = `${name}.html`;
+  const dirName = `${name}_files`;
+  return fs.access(dir, fs.constants.F_OK)
+    .then(() => fs.mkdir(path.resolve(dir, dirName)))
+    .then(() => loadData(address))
+    .then(data => transformData(data, dirName))
+    .then(({ formattedData, resourceLinks }) => {
+      const resourcePromises = resourceLinks.map(item => loadResources(`${address}${item}`, generateResourceDirName(path.resolve(dir, dirName), generateResourceName(item))));
+      return Promise.all(resourcePromises)
+        .then(() => fs.writeFile(path.resolve(dir, fileName), formattedData));
+    })
+    .catch(err => Promise.reject(new Error(err)));
 };
